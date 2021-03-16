a7aa594c778990046c04b9d06c8b7967d68b0923
Kevin Martinez-Chavez <martikev@uw.edu>
2021-03-15 21:12:16 -0700

    raised relative tolerance

--- examples/pyfiles_for_demos/pointclassifier.py
+++ examples/pyfiles_for_demos/pointclassifier.py
@@ -3,29 +3,33 @@
 
 def pointclassifier(userpoints):
     
+
     origin = []
     xpixelmax = []
     ypixelmax = []
     
+
     for i in range(len(userpoints)):
 
         ## classify which pixel point is the origin
-        if math.isclose(userpoints[i,0], min(userpoints[:,0]), rel_tol = 0.07 ) == True and math.isclose(userpoints[i,1], max(userpoints[:,1]), rel_tol = 0.07) == True:
+        if math.isclose(userpoints[i,0], min(userpoints[:,0]), rel_tol = 0.15 ) == True and math.isclose(userpoints[i,1], max(userpoints[:,1]), rel_tol = 0.15) == True:
             print(userpoints[i,:], "is the pixel location of the origin")
             origin = userpoints[i,:]
-    
-        ## classify which pixel point is the x max point on the x-axis coordinate
-        elif math.isclose(userpoints[i,0], max(userpoints[:,0]), rel_tol = 0.07 ) == True:
-            print(userpoints[i,:], "is the pixel location of the x-axis max")
-            xpixelmax = userpoints[i,:]
         
         ## classify which pixel point is the y max point on the y-axis coordinate
-        elif math.isclose(userpoints[i,0], min(userpoints[:,0]), rel_tol = 0.07 ) == True and math.isclose(userpoints[i,1], min(userpoints[:,1]), rel_tol = 0.07 ) == True:
+        elif math.isclose(userpoints[i,0], min(userpoints[:,0]), rel_tol = 0.15 ) == True and math.isclose(userpoints[i,1], min(userpoints[:,1]), rel_tol = 0.15 ) == True:
             print(userpoints[i,:], "is the pixel location of the y-axis max")
             ypixelmax = userpoints[i,:]
         
+        ## classify which pixel point is the x max point on the x-axis coordinate
+        elif math.isclose(userpoints[i,0], max(userpoints[:,0]), rel_tol = 0.15 ) == True:
+            print(userpoints[i,:], "is the pixel location of the x-axis max")
+            xpixelmax = userpoints[i,:]
+
         else:
             print(userpoints[i,:],"This point cannot be classified as one of the following: \n*Origin \n*X-coordinate max \n*y-coordinate max")
+            print("Please read notes and try selecting the points again")
             break
     
+
     return origin, xpixelmax, ypixelmax
